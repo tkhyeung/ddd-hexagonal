79a529d983448200d5ec82790309c34ea0e54ba0
hkinnkun <hkinnkun@gmail.com>
2021-07-18 00:20:58 +0800

    add performance monitor by aop
    1

---
+++ src/main/java/com/example/ddd/config/performance_monitor/PerformanceMonitor.java
@@ -0,0 +1,11 @@
+package com.example.ddd.config.performance_monitor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerformanceMonitor {
+}
