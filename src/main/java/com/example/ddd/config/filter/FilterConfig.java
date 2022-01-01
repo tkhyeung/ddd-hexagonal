f873bf1d9c7391258a2444bcfcea26dffa49a8ce
hkinnkun <hkinnkun@gmail.com>
2022-01-02 01:11:43 +0800

    comment out logging request response by Filter

--- src/main/java/com/example/ddd/config/filter/FilterConfig.java
+++ src/main/java/com/example/ddd/config/filter/FilterConfig.java
@@ -1,19 +1,19 @@
-package com.example.ddd.config.filter;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-    @Bean
-    public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter(){
-        FilterRegistrationBean<RequestResponseLoggingFilter> registrationBean
-                = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new RequestResponseLoggingFilter());
-        registrationBean.addUrlPatterns("/products/*");
-
-        return registrationBean;
-    }
-
-}
+//package com.example.ddd.config.filter;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FilterConfig {
+//    @Bean
+//    public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter(){
+//        FilterRegistrationBean<RequestResponseLoggingFilter> registrationBean
+//                = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new RequestResponseLoggingFilter());
+//        registrationBean.addUrlPatterns("/products/*");
+//
+//        return registrationBean;
+//    }
+//
+//}
